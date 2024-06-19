17a70bf673ce233fd8751f10f45e8cb47a0dbe3d
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-06-19 19:10:24 -0300

    att 1.7 - retirando tag P

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,18 @@ function selecionarAniversariante(aniversariante) {
     const resultado = document.getElementById('animacao');
     const edmar = 'src/img/img-edmar.png';
     const joao = 'src/img/img-jr.png';
+    const apagar = document.getElementById('frase');
 
     if(aniversariante === 'Edmar') {
         resultado.innerHTML = `<img src="${edmar}" alt="Edmar">`;
+        apagar.innerHTML = '';
+
     } else if(aniversariante === 'João Roberto') {
         resultado.innerHTML = `<img src="${joao}" alt="João Roberto" style="height 350">`;
+        apagar.innerHTML = '';
     } else {
         resultado.innerHTML = '';
+        apagar.innerHTML = 'Selecione o aniversariante:';
     }
 }
 
