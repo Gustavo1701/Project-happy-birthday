035da5cf4b10b0d742c1413ed51f8665a9db0dd8
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2024-06-18 23:23:30 -0300

    função de exibir imagem ao selecionar opção

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
-let foto;
-let resultado;
+
+function selecionarAniversariante(aniversariante) {
+    const resultado = document.getElementById('animacao');
+    const edmar = 'src/img/img-edmar.png';
+    const joao = 'src/img/img-jr.png';
+
+    if(aniversariante === 'Edmar') {
+        resultado.innerHTML = `<img src="${edmar}" alt="Edmar">`;
+    } else if(aniversariante === 'João Roberto') {
+        resultado.innerHTML = `<img src="${joao}" alt="João Roberto" style="height 350">`;
+    } else {
+        resultado.innerHTML = '';
+    }
+}
+
+
+
+
+
