b5b93435fc3c13d9eceba78513fb2a168a67d837
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-06-19 22:14:52 -0300

    att 1.8 - Resultado Final

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,51 @@
-
 function selecionarAniversariante(aniversariante) {
     const resultado = document.getElementById('animacao');
+    const mudarFrase = document.getElementById('frase');
     const edmar = 'src/img/img-edmar.png';
     const joao = 'src/img/img-jr.png';
-    const apagar = document.getElementById('frase');
+    const bgEdmar = 'src/img/balada-gay.jpg';
+    const bgJoao = 'src/img/robo3.png';
+    const somEdmar = document.getElementById('somEdmar');
 
-    if(aniversariante === 'Edmar') {
+    if (aniversariante === 'Edmar') {
         resultado.innerHTML = `<img src="${edmar}" alt="Edmar">`;
-        apagar.innerHTML = '';
-
-    } else if(aniversariante === 'João Roberto') {
-        resultado.innerHTML = `<img src="${joao}" alt="João Roberto" style="height 350">`;
-        apagar.innerHTML = '';
+        resultado.style.backgroundImage = `url('${bgEdmar}')`;
+        resultado.style.backgroundSize = 'cover'; 
+        mudarFrase.innerHTML = 'Parabéns No Filter';
+        criarConfetes(resultado);
+        somEdmar.play();
+    } else if (aniversariante === 'João Roberto') {
+        resultado.innerHTML = `
+            <div style="display: flex;">
+                <img src="${joao}" alt="João Roberto" style="height: 550px; flex: 1;">
+                <img src="${bgJoao}" alt="Fundo João Roberto" style="height: 350px; flex: 1;">
+            </div>`;
+        resultado.style.backgroundImage = 'None'
+        mudarFrase.innerHTML = 'Parabéns Tuê';
+        criarConfetes(resultado);
+        somEdmar.pause();
+        somEdmar.currentTime = 0;
     } else {
         resultado.innerHTML = '';
         apagar.innerHTML = 'Selecione o aniversariante:';
+        somEdmar.pause();
+        somEdmar.currentTime = 0;
     }
 }
 
+function criarConfetes(elementoPai) {
+    for (let i = 0; i < 50; i++) {
+        const confeti = document.createElement('div');
+        confeti.classList.add('confeti');
+        confeti.style.left = Math.random() * window.innerWidth + 'px';
+        confeti.style.animationDelay = Math.random() * 5 + 's';
+        elementoPai.appendChild(confeti);
 
+        setTimeout(() => {
+            confeti.remove();
+        }, 25000);
+    }
+}
 
 
 
